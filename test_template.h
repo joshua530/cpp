bb7865c4d4fcafb888d2be8f46b377cda1a9159f
joshua530 <joshuaomari5@gmail.com>
2021-12-24 09:19:46 +0300

    Add further explanation for template classes use

--- test_template.h
+++ test_template.h
@@ -15,3 +15,14 @@ private:
 #endif
 
 #include "test_template.cpp"
+
+/*
+ * classes that have template definitions and header files(defined in header,
+ * implemented in source file)
+ * --------------------------------------------------------------------------------------------------------
+ * -> define dummy function in implementation.cpp
+ * -> include implementation.cpp in file that uses the class(contains function
+ * or class that uses the class)
+ * -> include implementation.cpp in header file and remove implementation.cpp
+ * from project but not from folder
+ */
